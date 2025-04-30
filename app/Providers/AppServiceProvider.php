64234e7bab5223d58fec8d2dbfb9d320e47e552b
Robeir George <robier_samir@hotmail.co.uk>
2025-05-01 02:44:04 +0300

    Set the default string length for database migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Set the default string length for database migrations
+        // to avoid issues with MySQL's default string length limit
+        Schema::defaultStringLength(191);
     }
 }
